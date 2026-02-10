156e9f463cb47a58ef80cb6d59e2730b04aff9b4
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2026-02-10 14:59:43 +0000

    updating the sortable feature to get it working for student docs

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,6 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.use('/DSA-updates', require('./views/DSA-updates/_routes'));
+
 module.exports = router
