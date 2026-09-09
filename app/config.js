ea5ed9324cea0a367132c337679734ee982a2aef
Joe Hutchinson NHSBSA <jrhut@nhsbsa.nhs.uk>
2026-09-09 16:55:27 +0100

    Updated prototype and introduced indexes for student and internal

--- app/config.js
+++ app/config.js
@@ -1,16 +1,8 @@
 // Use this file to change prototype configuration.
-
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'Bursary',
 
-  // Port to run nodemon on locally
-  port: 2000,
-
-  // Automatically stores form data, and send to all views
-  useAutoStoreData: 'true',
-
-  // Enable cookie-based session store (persists on restart)
-  // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
-  useCookieSessionStore: 'false'
+  // Port to run the prototype on locally
+  port: 3000
 }
